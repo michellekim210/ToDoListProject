5834a69991686557328e3227c4eeb96eecf47e71
michellekim210 <69254574+michellekim210@users.noreply.github.com>
2020-08-10 21:36:40 -0400

    Add files via upload

---
+++ ToDoListProject/ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Michelle Kim on 8/10/20.
+//  Copyright © 2020 Michelle Kim. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
